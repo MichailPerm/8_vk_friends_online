5b810b5211a25735596733c2c0255b0083811fa5
Michail_Z <apxahrejl@gmail.com>
2018-05-05 17:59:53 +0500

    functions refactoring

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,25 +1,19 @@
-import vk
+import sys, vk, getpass
 
 
-def get_api_session(login, password):
-    app_id = 6469230
+def get_api_session(login, password, app_id=6469230, v='5.35'):
     session = vk.AuthSession(
         app_id=app_id,
         user_login=login,
         user_password=password,
         scope='friends'
     )
-    return vk.API(session, v='5.35')
+    return vk.API(session, v=v)
 
 
 def get_online_friends(api_session):
     friends_online_ids = api_session.friends.getOnline()
-    friends = api_session.friends.get(fields='online')['items']
-    friends_online = []
-    for friend in friends:
-        if friend['id'] in friends_online_ids:
-            friends_online.append(friend)
-    return friends_online
+    return api_session.users.get(user_ids=friends_online_ids)
 
 
 def output_friends_to_console(friends_online):
@@ -31,7 +25,10 @@ def output_friends_to_console(friends_online):
 
 if __name__ == '__main__':
     login = input('Input e-mail or phone number: ')
-    password = input('Imput your password: ')
-    api_session = get_api_session(login, password)
+    password = getpass.getpass(prompt='Input your password: ')
+    try:
+        api_session = get_api_session(login, password)
+    except vk.exceptions.VkAuthError:
+        sys.exit('You\'ve entered uncorrect login or password')
     friends_online = get_online_friends(api_session)
     output_friends_to_console(friends_online)
